b69889bb1d6d88477cc2748e85c355b28b34187a
Muppet9010 <mike.hollett@gmail.com>
2022-11-30 12:54:08 +0000

    v20.0.8
    Update to Factorio 1.1

--- prototypes/facility.lua
+++ prototypes/facility.lua
@@ -1,10 +1,10 @@
 local Utils = require("utility/utils")
 local Constants = require("constants")
-local sounds = require("__base__/prototypes/entity/demo-sounds")
+local sounds = require("__base__/prototypes/entity/sounds")
 
 local shopPaymentChest = Utils.DeepCopy(data.raw["logistic-container"]["logistic-chest-requester"])
 shopPaymentChest.name = "prime_intergalactic_delivery-shop_payment_chest"
-shopPaymentChest.flags = {"placeable-off-grid", "not-blueprintable", "not-deconstructable"}
+shopPaymentChest.flags = { "placeable-off-grid", "not-blueprintable", "not-deconstructable" }
 shopPaymentChest.minable = nil
 shopPaymentChest.inventory_size = settings.startup["prime_intergalactic_delivery-payment_chest_inventory_size"].value
 table.insert(
@@ -15,7 +15,7 @@ table.insert(
         size = 64,
         frame_count = 7,
         scale = 0.13,
-        shift = {0, 0.1}
+        shift = { 0, 0.1 }
     }
 )
 shopPaymentChest.render_not_in_network_icon = false
@@ -23,7 +23,7 @@ shopPaymentChest.order = "9999"
 
 local shopDeliveryChest = Utils.DeepCopy(data.raw["logistic-container"]["logistic-chest-passive-provider"])
 shopDeliveryChest.name = "prime_intergalactic_delivery-shop_delivery_chest"
-shopDeliveryChest.flags = {"placeable-off-grid", "not-blueprintable", "not-deconstructable"}
+shopDeliveryChest.flags = { "placeable-off-grid", "not-blueprintable", "not-deconstructable" }
 shopDeliveryChest.minable = nil
 shopDeliveryChest.render_not_in_network_icon = false
 shopDeliveryChest.order = "9999"
@@ -33,10 +33,10 @@ data:extend(
         {
             type = "market",
             name = "prime_intergalactic_delivery-shop_building",
-            flags = {"placeable-off-grid"},
-            collision_mask = {"item-layer", "object-layer", "player-layer", "water-tile", "resource-layer"},
-            collision_box = {{-1.4, -1.4}, {1.4, 2.4}},
-            selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+            flags = { "placeable-off-grid" },
+            collision_mask = { "item-layer", "object-layer", "player-layer", "water-tile", "resource-layer" },
+            collision_box = { { -1.4, -1.4 }, { 1.4, 2.4 } },
+            selection_box = { { -1.5, -1.5 }, { 1.5, 1.5 } },
             map_color = data.raw["utility-constants"].default.chart.default_friendly_color,
             vehicle_impact_sound = sounds.generic_impact,
             picture = {
@@ -45,15 +45,15 @@ data:extend(
                         filename = Constants.AssetModName .. "/graphics/entity/shop_building.png",
                         width = 156,
                         height = 127,
-                        shift = {0.95, 0.2}
+                        shift = { 0.95, 0.2 }
                     },
                     {
                         filename = Constants.AssetModName .. "/graphics/entity/shop_building_shadow.png",
-                        flags = {"shadow"},
+                        flags = { "shadow" },
                         draw_as_shadow = true,
                         width = 156,
                         height = 127,
-                        shift = {0.95, 0.2}
+                        shift = { 0.95, 0.2 }
                     }
                 }
             }
@@ -61,7 +61,7 @@ data:extend(
         {
             type = "simple-entity",
             name = "prime_intergalactic_delivery-shop_building_radar",
-            flags = {"placeable-off-grid", "not-selectable-in-game"},
+            flags = { "placeable-off-grid", "not-selectable-in-game" },
             render_layer = "higher-object-under",
             animations = {
                 layers = {
@@ -77,7 +77,7 @@ data:extend(
                     },
                     {
                         filename = Constants.AssetModName .. "/graphics/entity/shop_building_radar_shadow.png",
-                        flags = {"shadow"},
+                        flags = { "shadow" },
                         draw_as_shadow = true,
                         width = 172,
                         height = 94,
@@ -106,7 +106,7 @@ data:extend(
         {
             type = "simple-entity",
             name = "prime_intergalactic_delivery-shop_building_speaker",
-            flags = {"placeable-off-grid"},
+            flags = { "placeable-off-grid" },
             picture = data.raw["programmable-speaker"]["programmable-speaker"].sprite,
             water_reflection = data.raw["programmable-speaker"]["programmable-speaker"].water_reflection
         },
